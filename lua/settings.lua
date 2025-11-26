9ba605f34afb9db587fad4011279421bcc647a34
Ilcan Tiryaki <ilcantiryaki46@gmail.com>
2025-11-26 20:12:13 +0100

    always show signcolumn to avoid layout jump

--- lua/settings.lua
+++ lua/settings.lua
@@ -2,10 +2,9 @@ vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
-
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
-
 vim.o.relativenumber = true
+vim.o.signcolumn = "yes"
 
 
